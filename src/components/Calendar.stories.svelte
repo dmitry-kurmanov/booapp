9757f72cd6bf793ec7058e084795675b3a256ed4
dmitrykurmanov <kurmanov.work@gmail.com>
2021-04-19 23:35:09 +0300

    added first story and empty testcafe test;

--- src/components/Calendar.stories.svelte
+++ src/components/Calendar.stories.svelte
@@ -1,22 +1,30 @@
 <script>
-  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-  import Calendar from "./Calendar.svelte";
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Calendar from './Calendar.svelte';
 </script>
 
 <Meta
-  title="Example/Calendar"
-  component={Calendar}
-  argTypes={{
-    label: { control: "text" },
-    primary: { control: "boolean" },
-    backgroundColor: { control: "color" },
-    size: {
-      control: { type: "select", options: ["small", "medium", "large"] },
-    },
-    onClick: { action: "onClick" },
-  }}
+	title="Example/Calendar"
+	component={Calendar}
+	argTypes={{
+		label: { control: 'text' },
+		primary: { control: 'boolean' },
+		backgroundColor: { control: 'color' },
+		size: {
+			control: { type: 'select', options: ['small', 'medium', 'large'] },
+		},
+		onClick: { action: 'onClick' },
+	}}
 />
 
 <Template let:args>
-  <Calendar {...args} on:click={args.onClick} />
+	<Calendar {...args} on:click={args.onClick} />
 </Template>
+
+<Story
+	name="Primary"
+	args={{
+		primary: true,
+		label: 'Button',
+	}}
+/>
---
+++ src/components/Calendar.testcafe.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture `Calendar Component`
+    .page `http://127.0.0.1:6777`;
+
+test('My first test', async t => {
+    // Test code
+});
