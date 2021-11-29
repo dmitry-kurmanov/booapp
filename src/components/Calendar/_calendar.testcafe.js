742a2e824e6c4dac2b95d9f315489d3fa625fb32
dmitrykurmanov <kurmanov.work@gmail.com>
2021-11-29 20:59:11 +0300

    work for the #8

--- src/components/Calendar/_calendar.testcafe.js
+++ src/components/Calendar/_calendar.testcafe.js
@@ -22,8 +22,39 @@ test('select date', async (t) => {
 	await t.expect(FirstDayOfMonth.exists).ok();
 
 	await t.click(FirstDayOfMonth);
-	await t.expect(FirstDayOfMonth.hasClass("booapp-calendar__day--selected")).ok();
+	await t
+		.expect(FirstDayOfMonth.hasClass('booapp-calendar__day--selected'))
+		.ok();
 });
+
+// test('current date section', async (t) => {
+// 	const date = new Date();
+// 	const day = '' + date.getDate();
+// 	const fullYear = '' + date.getFullYear();
+
+// 	const currentDateSection = Selector('.booapp-calendar-current-date')
+// 		.withText(day)
+// 		.withText(fullYear);
+
+// 	await t.expect(currentDateSection.exists).ok();
+
+// 	const FirstDayOfMonth = Selector('.booapp-calendar__day').withText('1');
+// 	await t.click(FirstDayOfMonth);
+// 	await t.expect(currentDateSection.exists).ok();
+// });
+
+// test('selected date section', async (t) => {
+// 	const selectedDateSection = Selector('.booapp-calendar-selected-date');
+
+// 	const CurrentDayOfMonth = Selector('.booapp-calendar__day--current');
+// 	const FirstDayOfMonth = Selector('.booapp-calendar__day').withText('1');
+
+// 	await t.click(CurrentDayOfMonth);
+// 	await t.expect(selectedDateSection.withText('today').exists).ok();
+
+// 	await t.click(FirstDayOfMonth);
+// 	await t.expect(selectedDateSection.withText('1').exists).ok();
+// });
 
 test('Screenshot testing (visual)', async (t) => {
 	await t.takeElementScreenshot(
